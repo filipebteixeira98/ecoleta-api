d56144649162cc198129b52564c143d02d8ba47c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-12-07 11:32:38 -0300

    chore: configured database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.send();
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server is running at http://localhost:3333');
