92b47d9baefd539797a0131aef4826bb5f6541d7
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-12-08 17:25:05 -0300

    feat: add static route to get images & items info

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(routes);
 
