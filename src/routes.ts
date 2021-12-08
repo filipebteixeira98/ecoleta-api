c92444f9920adc6be9f906d17599665cd81ac068
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-12-08 18:04:00 -0300

    chore: created ItemsController

--- src/routes.ts
+++ src/routes.ts
@@ -2,24 +2,15 @@ import express from 'express';
 
 import knex from './database/connection';
 
+import ItemsController from './controllers/ItemsController';
 import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
 
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
-
-  const serializedItems = items.map(item => {
-    return {
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-
-  return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
 
