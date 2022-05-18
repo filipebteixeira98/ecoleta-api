f78921239fe61565c3ba8de17ad0d14618c2847c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-05-18 05:25:44 -0300

    chore: added middleware which upload to disk storage

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import ItemsController from './controllers/ItemsController';
 import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
+
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
@@ -12,6 +17,6 @@ routes.get('/items', itemsController.index);
 
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
-routes.post('/points', pointsController.create);
+routes.post('/points', upload.single('image'), pointsController.create);
 
 export default routes;
